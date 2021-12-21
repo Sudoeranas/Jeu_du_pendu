3fcf8bce23cf7e1ada6e53583faef8a908229fe2
Anas IKLI <92299578+Sudoeranas@users.noreply.github.com>
2021-12-22 00:17:56 +0100

    Update Jeu_du_pendu.c

--- Jeu_du_pendu.c
+++ Jeu_du_pendu.c
@@ -10,7 +10,7 @@ int main(){
 	char lettre[MAX];
 	char chaine_mystere[MAX];
 	int nb_essais = 10;
-	int longueur, existence, i=1;
+	int *longueur, existence, i=1;
 	while(chaine[i]!='\0'){
 		i++;
 		} 
@@ -24,7 +24,7 @@ int main(){
 		printf("Il vous reste %d coups à jouer \n", nb_essais);
 		printf("Quel est le mot secret ? ");
 
-		puts(chaine_mystere);
+		printf("%s", chaine_mystere);
 
 		printf("\nProposez une lettre : ");
 		gets(lettre);
