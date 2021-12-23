1fd586afe541fa1628027017d2e6d9e0249eba0e
Anas IKLI <92299578+Sudoeranas@users.noreply.github.com>
2021-12-23 12:27:57 +0100

    Update Jeu_du_pendu.c

--- Jeu_du_pendu.c
+++ Jeu_du_pendu.c
@@ -5,33 +5,32 @@
 #define	MAX	80
 char checker(char *ch);
 
-int main(){
-	const char chaine[MAX] = "rouge" ;
+int main(int argc, char **argv[]){
+	const char chaine[MAX] = "printemps" ;
 	char lettre[MAX];
 	char chaine_mystere[MAX];
 	int nb_essais = 10;
-	int *longueur, existence, i=1;
-	while(chaine[i]!='\0'){
-		i++;
+	int longueur, existence, i=0, k=0;
+	while(chaine[k]!='\0'){
+		k++;
 		} 
-	longueur=i ;	
-	for (int i=0; i<longueur-2; i++){
+	longueur=k ;
+	for (i=0; i<longueur; i++){
 		chaine_mystere[i] = '*';
 		}
+	chaine_mystere[longueur]=' ';
 	printf("Bienvenue dans le Pendu !\n\n");
-	
+
 	while(checker(chaine_mystere)==1 && nb_essais>0) {
-		printf("Il vous reste %d coups à jouer \n", nb_essais);
+		printf("\nIl vous reste %d coups à jouer \n", nb_essais);
 		printf("Quel est le mot secret ? ");
-
-		printf("%s", chaine_mystere);
-
+		puts(chaine_mystere);
 		printf("\nProposez une lettre : ");
 		gets(lettre);
 		// tout rendre minuscule ou majuscule
 		for (int j=0;j<longueur;j++){
-			if (lettre==chaine[j]){
-				chaine_mystere[j]=lettre;
+			if (lettre[0]==chaine[j]){
+				chaine_mystere[j]=lettre[0];
 				existence = 1;
 				break;
 				}
@@ -40,8 +39,9 @@ int main(){
 				}			
 			}
 		if (existence==0)
-			nb_essais--;
+			nb_essais=nb_essais-1;	
 		}
+	printf("Félicitations ! vous avez trouvé le mot %s ! Vous êtes bien dans ce jeu ! \n", chaine_mystere);
 	}
 	
 char checker(char *ch){
